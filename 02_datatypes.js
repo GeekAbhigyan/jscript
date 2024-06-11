ddd1ba6c33cba8db2afa85b813b6156c5c179684
Abhigyan <120541586+GeekAbhigyan@users.noreply.github.com>
2024-06-11 20:08:29 +0000

    datatypes

---
+++ 02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict";// treat all js code as newer version
+
+//alert() // we are using node , not browser 
+
+//code readablity is important for future references
+
+//ecma script is the standard for the javascript
+
+let name = "abhi";
+let age = 23;
+let isLogged= false;
+
+//data types 
+
+//number 
+//bigint-> large number 
+//boolean-> 2 respnses
+//null -> standalone value ( empty value)
+//undefined -> ualue not assined yet 
+//symbol -> uniquness provide 
+//objects 
+
+console.log(typeof null) //object
+console.log(typeof undefined) // undefined 
