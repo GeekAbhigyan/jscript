ea231f96dac110afc238c62427fb1d8a1b1f7ce0
Abhigyan <120541586+GeekAbhigyan@users.noreply.github.com>
2024-06-12 08:18:18 +0000

    comparison of datatypes

---
+++ 04comparison.js
@@ -0,0 +1,20 @@
+//try to compare between the same datatypes 
+
+console.log("2">1); // automatially converts into numbers
+console.log("02">1);
+
+//comparison between null and zero
+
+console.log(null>0);
+console.log(null==0);
+console.log(null >=0);
+
+
+// == and >= work differently
+
+// wheres == , > ,  < treats null as NaN
+// >= treat null as 0 and then give the answer 
+
+//==only checks for value 
+
+// === also checks for the datatype as well
