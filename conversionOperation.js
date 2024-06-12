b90dcb5c7191aba0c1d1e025dcef46e344eb7967
Abhigyan <120541586+GeekAbhigyan@users.noreply.github.com>
2024-06-12 07:11:43 +0000

    conversion of staged changes

---
+++ conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "33b4C"
+let num = "23"
+
+console.log(typeof score)
+console.log(typeof num)
+let valueInNum = Number(num)
+
+console.log(typeof valueInNum)
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1 ; false => 0;
+
+let isLoggenIn =1;
+let booleanIsLoggedIn =Boolean(isLoggenIn);
+console.log(booleanIsLoggedIn); 
+
+// 1 => true ; 0 => false
+//""=> false 
+// "abhi"=> true
+
+/* conversion to string */
+
+let numb = 23;
+
+let stringNumb = String(numb);
+console.log(typeof stringNumb);
