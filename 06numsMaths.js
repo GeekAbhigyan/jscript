241ff80e06771f66c1f458be527fac0514318636
Abhigyan <120541586+GeekAbhigyan@users.noreply.github.com>
2024-06-14 18:58:43 +0000

    number and maths

---
+++ 06numsMaths.js
@@ -0,0 +1,65 @@
+/********************* to remember as notes  */
+const mina= 10 ;
+const maxa = 20 ;
+
+console.log(Math.floor(Math.random() * (maxa -mina +1))+ mina);
+ 
+/******************************************** */
+const score = 19;
+console.log (score )
+
+// dedicatedly a number 
+
+const balance = new Number(500);
+console.log(balance);
+
+console.log(balance.toString().length);
+
+// important in e-commerce app building
+console.log(balance.toFixed(2));
+console.log(balance.toPrecision(3))
+
+let num = 1242.3456789;
+console.log(num.toPrecision(3));
+// ans -> 1.24e+3
+// take care of the exponential case in the precision 
+
+let hundreads = 10000000000;
+console.log(hundreads.toLocaleString('en-IN'));
+
+// comma lagane ke kaam hota hai -> to local string 
+
+
+let mi = Number.MIN_VALUE;
+console.log(mi);
+
+
+/**********************************maths************* */
+console.log(`******************************************`)
+
+console.log(Math.abs(-4));
+console.log(Math.round(4.5789));
+
+// ceil and floor value ***** // leetcode contest me aaya tha 
+
+
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.2));
+
+// min value in a array 
+console.log(Math.min(1,2,3,4,5,6))
+
+// math .random -> gives random value b/w 0,1
+console.log(Math.random());
+console.log(Math.random() *10 +1 ); // 1-10 tak ki values
+
+const min= 10 ;
+const max = 20 ;
+
+console.log(Math.floor(Math.random() * (max -min +1))+ min);
+ 
+
+
+
+
+
