536602c2ce5468e04b7c8c09cc7b01fe1fdf5f6c
Abhigyan <120541586+GeekAbhigyan@users.noreply.github.com>
2024-06-23 17:12:57 +0530

    json done

---
+++ objects4.js
@@ -0,0 +1,33 @@
+const course = {
+    coursename: 'js in hindi',
+    price:'99',
+    courseinstructor:'hitesh'
+}
+
+//course.courseinstructor
+
+// destructuring
+
+const {courseinstructor:instructor}= course;
+
+console.log(courseinstructor);
+
+const navbar = () => {
+
+}
+
+navbar{company='hitesh'}
+
+// api -> apna kaam dusre ke sar pe de dena 
+
+// api se values aati hai jason ke form me 
+
+ {
+    "nam": "hit",
+    "course": "js",
+    "price":"free"
+ }
+
+
+ //json -> javascript object notation 
+ // has something to do with api
