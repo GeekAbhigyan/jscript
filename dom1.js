d7b1453db7757c6e6584aab271d630acb841b73d
Abhigyan <singhdefinite12@gmail.com>
2024-07-04 19:54:09 +0530

    dom manipulation

---
+++ dom1.js
@@ -0,0 +1,38 @@
+// difference between inner text inner html and text content 
+
+/* innerHTML gives the the whole html code of the specified id 
+
+
+innertext gives the text that is visible on the screen 
+
+
+textcontent gives the whole of the content available on the html code weather it is hidden or shown on thr webpage
+
+*/
+/* 
+
+queryselectorall me node list milta hai 
+
+array nahi 
+
+to foreach se lop lagega 
+
+
+
+*/ 
+/**
+ 
+getelementbyclassName me Html collection milta hai 
+
+ */
+
+/**node list le uppar map use karna ho to ussko convert kar lenge arrays me 
+ * 
+ * waise hi html collection pe for each use karne ke liye ussko maps me convert karn parega
+ * 
+ */
+
+
+
+
+
