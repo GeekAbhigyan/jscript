db5779e623effb7d6a2a841398b0b23178140d94
Abhigyan <singhdefinite12@gmail.com>
2024-07-09 22:38:19 +0530

    fetch

--- 09advance/promises.js
+++ 09advance/promises.js
@@ -115,5 +115,26 @@ async function consumepromisefive(){
 consumepromisefive()
 
 async function getAllUsers(){
-    const response = await fetch()
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/users")
+const data = await response.json();
+console.log(data);
+
+        
+    } catch (error) {
+        console.log("error");
+        
+    }
 }
+getAllUsers();
+
+fetch("https://jsonplaceholder.typicode.com/users")
+.then((response)=>{
+    return response.json()
+})
+.then((data)=>{
+    console.log(data)
+})
+.catch((error)=>{
+    console.log(error)
+})
